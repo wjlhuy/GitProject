183506ac092433e6dc77877f0e2e8abf3985f31f
wjlhuy <73728628+wjlhuy@users.noreply.github.com>
2023-10-17 11:56:11 +0700

    Add files via upload

---
+++ 7bai2.js
@@ -0,0 +1,6 @@
+inputWidth = prompt("nhập chiều rộng");
+inputHeight = prompt("Nhập chiều cao");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Diện tích là: " + area);
